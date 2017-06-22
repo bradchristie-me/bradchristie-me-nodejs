7fd92c9279c8dc0a5201da53566e1a2407a354f7
Brad Christie <bradchristie@bradchristie.me>
2017-06-22 16:05:55 -0400

    cleanup

--- dist/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const app = express();
-app.use('/', (req, res) => {
-    res.send('Hello, world!');
-});
-exports.default = app;
-//# sourceMappingURL=server.js.map
